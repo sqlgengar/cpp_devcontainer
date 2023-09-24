13e26deb703776177ea0bb076c80db7759262c72
alejandrothinkion <89306401+sqlgengar@users.noreply.github.com>
2023-09-24 01:20:03 -0300

    skecht interseccion listas

--- main.c
+++ main.c
@@ -1,248 +1,162 @@
-/**
- *  Desarrollar menu de operaciones basicas junto con cola invertida.
-**/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct data
-{
+typedef struct data {
     int number;
     struct data* next;
 } Node;
 
-typedef struct
-{
-    Node* front;
-    Node* rear;
-} Queue;
+typedef struct {
+    Node* head;
+} List;
 
 void execute();
-Queue* show_menu( Queue* );
-Queue* resolve_option( int, Queue* );
-Queue* create_queue( bool );
-Queue* enqueue( Queue*, int, bool );
-Queue* dequeue( Queue*, bool );
-bool is_empty( Queue*, bool );
-Queue* show_queue( Queue* );
-Queue* sort_queue( Queue* );
-int get_front_value( Queue*, bool );
-Queue* delete_queue( Queue* );
+List* show_menu(List*);
+List* resolve_option(int, List*);
+List* create_list(bool);
+List* insert(List*, int, bool);
+bool is_empty(List*, bool);
+List* show_list(List*);
+List* intersection(List*, List*);
 
-int main()
-{
-  execute();
-  return 0;
+int main() {
+    execute();
+    return 0;
 }
 
-void execute()
-{
-  Queue* queue = NULL;
+void execute() {
+    List* list1 = NULL;
+    List* list2 = NULL;
 
-  while( true )
-  {
-    queue = show_menu(queue);
-  }
+    while (true) {
+        list1 = show_menu(list1);
+        list2 = show_menu(list2);
+    }
 }
 
-Queue* show_menu( Queue* queue )
-{
-  int temp_option = -1;
+List* show_menu(List* list) {
+    int temp_option = -1;
 
-  printf( "\n\n" );
-  printf( "Ingrese el numero de opcion:\n" );
-  printf( "Crear cola:                   (1)\n" );
-  printf( "Agregar elemento a la cola:   (2)\n" );
-  printf( "Sacar elemento de la cola:    (3)\n" );
-  printf( "Listar cola:                  (4)\n" );
-  printf( "Ordenar cola:                 (5)\n" );
-  printf( "Eliminar la cola:             (6)\n" );
-  printf( "Salir:                        (7)\n" );
-  printf( "\n\n" );
+    printf("\n\n");
+    printf("Ingrese el numero de opcion:      \n");
+    printf("Crear lista:                   (1)\n");
+    printf("Insertar elemento en la lista: (2)\n");
+    printf("Mostrar lista:                 (3)\n");
+    printf("Calcular intersección:         (4)\n");
+    printf("Salir:                         (5)\n");
+    printf("\n\n");
 
-  scanf( " %d", &temp_option );
-  fflush( stdin );
+    scanf(" %d", &temp_option);
+    fflush(stdin);
 
-  return resolve_option( temp_option, queue );
+    return resolve_option(temp_option, list);
 }
 
-Queue* resolve_option( int option, Queue* queue )
-{
-  int input_user_value =  NULL;
-  bool verbose =          true;
+List* resolve_option(int option, List* list) {
+    int input_user_value = NULL;
+    bool verbose = true;
 
-  switch( option )
-  {
-    case 1:
-      queue = create_queue( verbose );
-    break;
-    case 2:
-      queue = enqueue( queue, input_user_value, verbose );
-    break;
-    case 3:
-      queue = dequeue( queue, verbose );
-    break;
-    case 4:
-      queue = show_queue( queue );
-    break;
-    case 5:
-      queue = sort_queue( queue );
-    break;
-    case 6:
-      queue = delete_queue( queue );
-    break;
-    case 7:
-      exit( EXIT_SUCCESS );
-    break;
-  }
-
-  return queue;
-}
-
-Queue* create_queue( bool is_verbose )
-{
-  Queue* temp_queue = (Queue*)malloc( sizeof( Queue ) );
-  temp_queue->front = NULL;
-  temp_queue->rear =  NULL;
-
-  if( is_verbose ) printf( "Se creo la cola %p\n", (void*)temp_queue );
-
-  return temp_queue;
-}
-
-Queue* enqueue( Queue* queue, int aux_value, bool is_verbose )
-{
-  int temp_number = aux_value;
-  Node* new_node =  (Node*)malloc( sizeof( Node ) );
-
-  if( temp_number == NULL )
-  {
-    printf( "Ingrese valor:\n" );
-    scanf( " %d", &temp_number );
-    fflush( stdin );
-  }
-
-  new_node->number =  temp_number;
-  new_node->next =    NULL;
-
-  // Si la cola esta vacia el principio y el final hacen referencia al mismo elemento.
-  if( is_empty( queue, is_verbose ) )
-  {
-    queue->front =  new_node;
-    queue->rear =   new_node;
-  }
-
-  // Actualiza las referencias del final de la cola para agregarlo.
-  queue->rear->next = new_node;
-  queue->rear =       new_node;
-
-  if( is_verbose ) printf( "Se encolo el nodo %p y valor de memoria %d \n", (void*)new_node, get_front_value( queue, is_verbose ) );
-
-  return queue;
-}
-
-Queue* dequeue( Queue* queue, bool is_verbose )
-{
-  if( is_empty( queue, is_verbose ) ) return queue;
-
-  int value_removed =   get_front_value( queue, is_verbose );
-  Node* node_removed =  queue->front;
-
-  // Chequeamos si es el unico elemento en la cola
-  if( queue->front == queue->rear )
-  {
-    queue->front =  NULL;
-    queue->rear =   NULL;
-  }
-  if( queue->front != queue->rear ) queue->front = queue->front->next;
-
-  free( node_removed );
-
-  if( is_verbose ) printf( "Se desencolo el nodo %p y valor de memoria %d \n", (void*)node_removed, value_removed );
-
-  return queue;
-}
-
-bool is_empty( Queue* queue, bool is_verbose )
-{
-  if( queue->front != NULL ) return false;
-
-  if( is_verbose ) printf( "Cola vacia \n" );
-  return true;
-}
-
-Queue* show_queue( Queue* queue )
-{
-  bool verbose = true;
-
-  if( is_empty( queue, verbose ) ) return queue;
-
-  Node* current = queue->front;
-
-  printf( "Contenido de la cola: \n" );
-  while( current != NULL )
-  {
-    printf("%d \n", current->number);
-    current = current->next;
-  }
-
-  return queue;
-}
-
-Queue* sort_queue( Queue* queue )
-{
-  bool no_verbose =     false;
-  Queue* sorted_queue = create_queue( no_verbose );
-
-  while( !is_empty( queue, no_verbose ) )
-  {
-    int current = get_front_value( queue, no_verbose );
-    dequeue( queue, no_verbose );
-
-    while( !is_empty( sorted_queue, no_verbose ) && get_front_value( sorted_queue, no_verbose ) > current )
-    {
-      int front_value = get_front_value( sorted_queue, no_verbose );
-      dequeue( sorted_queue, no_verbose );
-      enqueue( queue, front_value, no_verbose );
+    switch (option) {
+        case 1:
+            list = create_list(verbose);
+            break;
+        case 2:
+            printf("Ingrese valor a insertar: ");
+            scanf(" %d", &input_user_value);
+            list = insert(list, input_user_value, verbose);
+            break;
+        case 3:
+            list = show_list(list);
+            break;
+        case 4:
+            // Calculate intersection
+            if (list != NULL) {
+                List* other_list = create_list(verbose);
+                printf("Ingrese valores para la segunda lista. Finalice con -1:\n");
+                int value;
+                while (true) {
+                    scanf(" %d", &value);
+                    if (value == -1) break;
+                    other_list = insert(other_list, value, verbose);
+                }
+                list = intersection(list, other_list);
+                free(other_list);
+            } else {
+                printf("La primera lista está vacía. Crea una lista primero.\n");
+            }
+            break;
+        case 5:
+            exit(EXIT_SUCCESS);
+            break;
     }
 
-    enqueue( sorted_queue, current, no_verbose );
-  }
-
-  while( !is_empty( sorted_queue, no_verbose ) )
-  {
-    int front_value = get_front_value( sorted_queue, no_verbose );
-    dequeue( sorted_queue, no_verbose );
-    enqueue( queue, front_value, no_verbose );
-  }
-
-  printf( "Cola ordenada\n" );
-
-  return queue;
+    return list;
 }
 
-int get_front_value( Queue* queue, bool is_verbose )
-{
-  if( is_empty( queue, is_verbose ) ) return 0;
+List* create_list(bool is_verbose) {
+    List* new_list = (List*)malloc(sizeof(List));
+    new_list->head = NULL;
 
-  return queue->front->number;
+    if (is_verbose) printf("Se creó la lista %p\n", (void*)new_list);
+
+    return new_list;
 }
 
-Queue* delete_queue( Queue* queue )
-{
-  bool verbose =    true;
-  bool no_verbose = false;
+List* insert(List* list, int value, bool is_verbose) {
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    new_node->number = value;
+    new_node->next = list->head;
+    list->head = new_node;
 
-  if( is_empty( queue, verbose ) ) return queue;
+    if (is_verbose) printf("Se insertó el valor %d en la lista\n", value);
 
-  while( !is_empty( queue, no_verbose ) )
-  {
-    dequeue(queue, no_verbose);
-  }
+    return list;
+}
 
-  free(queue);
+bool is_empty(List* list, bool is_verbose) {
+    if (list->head != NULL) return false;
 
-  printf( "La cola ha sido eliminada\n" );
-  return NULL;
+    if (is_verbose) printf("La lista está vacía\n");
+    return true;
+}
+
+List* show_list(List* list) {
+    bool verbose = true;
+
+    if (is_empty(list, verbose)) return list;
+
+    Node* current = list->head;
+
+    printf("Contenido de la lista:\n");
+
+    while (current != NULL) {
+        printf("%d\n", current->number);
+        current = current->next;
+    }
+
+    return list;
+}
+
+List* intersection(List* list1, List* list2) {
+    bool no_verbose = false;
+    List* result = create_list(no_verbose);
+
+    Node* current1 = list1->head;
+    while (current1 != NULL) {
+        int value = current1->number;
+        Node* current2 = list2->head;
+        while (current2 != NULL) {
+            if (current2->number == value) {
+                result = insert(result, value, no_verbose);
+                break;
+            }
+            current2 = current2->next;
+        }
+        current1 = current1->next;
+    }
+
+    printf("Intersección calculada y almacenada en una nueva lista.\n");
+
+    return result;
 }
