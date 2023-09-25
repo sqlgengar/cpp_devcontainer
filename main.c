1cd837ac22f3d3d13a1abc3e08946f16586f6d0f
alejandrothinkion <89306401+sqlgengar@users.noreply.github.com>
2023-09-24 22:16:21 -0300

    skect listas

--- main.c
+++ main.c
@@ -1,162 +1,212 @@
+/**
+ *
+ *  Desarrollar interseccion de 2 listas.
+ *
+**/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct data {
-    int number;
-    struct data* next;
+typedef struct data
+{
+  int number;
+  int count;
+  struct data* next;
 } Node;
 
-typedef struct {
-    Node* head;
+typedef struct
+{
+  Node* top;
 } List;
 
 void execute();
-List* show_menu(List*);
-List* resolve_option(int, List*);
-List* create_list(bool);
-List* insert(List*, int, bool);
-bool is_empty(List*, bool);
-List* show_list(List*);
-List* intersection(List*, List*);
+List* show_menu( List* );
+List* resolve_option( int, List* );
+List* create_list( bool );
+List* push( List*, int, bool );
+List* pop( List*, bool );
+bool is_empty( List*, bool );
+List* show_list( List* );
+//List* join_list( List* );
+int get_top_value( List*, bool );
+int resolve_value_pop( int );
+List* delete_list( List* );
 
-int main() {
-    execute();
-    return 0;
+int main()
+{
+  execute();
+  return 0;
 }
 
-void execute() {
-    List* list1 = NULL;
-    List* list2 = NULL;
+void execute()
+{
+  List* list = NULL;
 
-    while (true) {
-        list1 = show_menu(list1);
-        list2 = show_menu(list2);
-    }
+  while( true )
+  {
+    list = show_menu( list );
+  }
+
+  return;
 }
 
-List* show_menu(List* list) {
-    int temp_option = -1;
+List* show_menu( List* list )
+{
+  int temp_option = -1;
 
-    printf("\n\n");
-    printf("Ingrese el numero de opcion:      \n");
-    printf("Crear lista:                   (1)\n");
-    printf("Insertar elemento en la lista: (2)\n");
-    printf("Mostrar lista:                 (3)\n");
-    printf("Calcular intersección:         (4)\n");
-    printf("Salir:                         (5)\n");
-    printf("\n\n");
+  printf("\n\n");
+  printf( "Ingrese el numero de opcion: \n" );
+  printf( "Crear lista:                 (1)\n" );
+  printf( "Agregar elemento a la lista: (2)\n" );
+  printf( "Sacar elemento de la lista:  (3)\n" );
+  printf( "Listar lista:                (4)\n" );
+  printf( "Ordenar lista:               (5)\n" );
+  printf( "Eliminar la lista:           (6)\n" );
+  printf( "Salir:                       (7)\n" );
+  printf("\n\n");
 
-    scanf(" %d", &temp_option);
-    fflush(stdin);
+  scanf( " %d", &temp_option );
+  fflush( stdin );
 
-    return resolve_option(temp_option, list);
+  return resolve_option( temp_option, list );
 }
 
-List* resolve_option(int option, List* list) {
-    int input_user_value = NULL;
-    bool verbose = true;
+List* resolve_option( int option, List* list )
+{
+  int input_user_value =  NULL;
+  bool verbose =          true;
 
-    switch (option) {
-        case 1:
-            list = create_list(verbose);
-            break;
-        case 2:
-            printf("Ingrese valor a insertar: ");
-            scanf(" %d", &input_user_value);
-            list = insert(list, input_user_value, verbose);
-            break;
-        case 3:
-            list = show_list(list);
-            break;
-        case 4:
-            // Calculate intersection
-            if (list != NULL) {
-                List* other_list = create_list(verbose);
-                printf("Ingrese valores para la segunda lista. Finalice con -1:\n");
-                int value;
-                while (true) {
-                    scanf(" %d", &value);
-                    if (value == -1) break;
-                    other_list = insert(other_list, value, verbose);
-                }
-                list = intersection(list, other_list);
-                free(other_list);
-            } else {
-                printf("La primera lista está vacía. Crea una lista primero.\n");
-            }
-            break;
-        case 5:
-            exit(EXIT_SUCCESS);
-            break;
-    }
+  switch( option )
+  {
+    case 1:
+      list = create_stack( verbose );
+    break;
+    case 2:
+      list = push( list, input_user_value, verbose );
+    break;
+    case 3:
+      list = pop( list, verbose );
+    break;
+    case 4:
+      list = show_stack( list );
+    break;
+    case 5:
+      list = sort_stack( list );
+    break;
+    case 6:
+      list = delete_stack( list );
+    break;
+    case 7:
+      exit( EXIT_SUCCESS );
+    break;
+  }
 
-    return list;
+  return list;
 }
 
-List* create_list(bool is_verbose) {
-    List* new_list = (List*)malloc(sizeof(List));
-    new_list->head = NULL;
+List* create_stack( bool is_verbose )
+{
+  List* temp_list = ( List* )malloc( sizeof( List ) );
+  temp_list->top =   NULL;
+  
+  if( is_verbose ) printf( "Se creo la lista %p\n", (void*)temp_list );
 
-    if (is_verbose) printf("Se creó la lista %p\n", (void*)new_list);
-
-    return new_list;
+  return temp_list;
 }
 
-List* insert(List* list, int value, bool is_verbose) {
-    Node* new_node = (Node*)malloc(sizeof(Node));
-    new_node->number = value;
-    new_node->next = list->head;
-    list->head = new_node;
+List* push( List* list, int aux_value, bool is_verbose )
+{
+  int temp_number = aux_value;
+  Node* node =      NULL;
 
-    if (is_verbose) printf("Se insertó el valor %d en la lista\n", value);
+  node = (Node*)malloc( sizeof( Node ) );
+  
+  if( temp_number == NULL )
+  {
+    printf( "Ingrese valor:\n" );
+    scanf( " %d", &temp_number );
+    fflush( stdin );
+  }
 
-    return list;
+  node->number =    temp_number;
+  node->next =      NULL;
+    
+  node->next =  list->top;
+  list->top =   node;
+
+  if( is_verbose ) printf( "Se creo el nodo %p y valor de memoria %d \n", (void*)node, get_top_value( list, is_verbose ) );
+
+  return list;
 }
 
-bool is_empty(List* list, bool is_verbose) {
-    if (list->head != NULL) return false;
+List* pop( List* list, bool is_verbose )
+{
+  int value_erase;
+  Node* memory_erase;
 
-    if (is_verbose) printf("La lista está vacía\n");
-    return true;
+  if( is_empty( list, is_verbose ) ) return list;
+
+  Node* temp =    list->top;
+  value_erase =   get_top_value( list, is_verbose );
+  memory_erase =  temp;
+
+  list->top =    temp->next;
+  free( temp );
+
+  if( is_verbose ) printf( "Se borro el nodo %p y valor de memoria %d \n", (void*)memory_erase, value_erase );
+
+  return list;
 }
 
-List* show_list(List* list) {
-    bool verbose = true;
+bool is_empty( List* list, bool is_verbose )
+{
+  if( list->top != NULL ) return false;
 
-    if (is_empty(list, verbose)) return list;
-
-    Node* current = list->head;
-
-    printf("Contenido de la lista:\n");
-
-    while (current != NULL) {
-        printf("%d\n", current->number);
-        current = current->next;
-    }
-
-    return list;
+  if( is_verbose ) printf( "Pila vacia \n" );
+  return true;
 }
 
-List* intersection(List* list1, List* list2) {
-    bool no_verbose = false;
-    List* result = create_list(no_verbose);
+List* show_stack( List* list )
+{
+  bool verbose = true;
 
-    Node* current1 = list1->head;
-    while (current1 != NULL) {
-        int value = current1->number;
-        Node* current2 = list2->head;
-        while (current2 != NULL) {
-            if (current2->number == value) {
-                result = insert(result, value, no_verbose);
-                break;
-            }
-            current2 = current2->next;
-        }
-        current1 = current1->next;
-    }
+  if( is_empty( list, verbose ) ) return list;
 
-    printf("Intersección calculada y almacenada en una nueva lista.\n");
+  Node* current = list->top;
+  
+  printf( "Contenido de la lista: \n" );
 
-    return result;
+  while( current != NULL )
+  {
+      printf( "%d \n", current->number );
+      current = current->next;
+  }
+
+  return list;
+}
+
+int get_top_value( List* list, bool is_verbose )
+{
+  if( is_empty( list, is_verbose ) ) return 0;
+  
+  return list->top->number;
+}
+
+List* delete_stack( List* list )
+{
+  bool verbose = true;
+  bool no_verbose = false;
+
+  if( is_empty( list, verbose ) ) return list;
+
+  while( !is_empty( list, no_verbose ) )
+  {
+    pop( list, no_verbose );
+  }
+
+  free( list );
+
+  printf( "La lista ha sido eliminada\n" );
+  return NULL;
 }
